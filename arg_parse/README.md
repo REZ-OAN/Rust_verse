a42cecfce47cd2bc2e0dd45da2a5674dbcaafff2
REZ-OAN <ahmedabir.rez@gmail.com>
2024-04-28 00:23:32 +0600

    added detailed example of arg_parsing using clap in rust command > subcommand > values

--- arg_parse/README.md
+++ arg_parse/README.md
@@ -11,3 +11,62 @@ clap is a the most popular argument libary for rust to handle the CLI
 cargo add clap --feature derive
 ```
 You just have to define the structure of the arguments and clap will implement the logic of be half of you
+
+Here we implemented an arg_parser for the below structure
+
+```
+command
+   | user
+   | todo
+subcommand
+    
+    | for user 
+        | - create
+        | - update
+        | - delete
+        | - show
+    | for todo    
+        | - create
+        | - update
+        | - delete
+        | - show
+required values
+    | for user
+        | - create 
+            | - name
+            | - email
+            | - password
+        | - update
+            | - email
+            | - new_name
+            | - new_password
+            | - old_password
+        | - delete
+            | - email
+        | - show
+            | - email
+    | for todo 
+        | - create
+            | - index
+            | - description
+            | - status
+        | - update
+            | - index
+            | - status
+            | - new_description
+        | - delete
+            | - index
+        | - show 
+            | - index
+```
+
+## How CLI looks like  of our implementation
+For the commands `user` or `todo`
+![Packet Encapsulation](https://github.com/REZ-OAN/Rust_verse/blob/arg_parse/images/commands.png)
+For the subcommands of `user`
+![Packet Encapsulation](https://github.com/REZ-OAN/Rust_verse/blob/arg_parse/images/user_subcommands.png)
+For the subcommand `create`
+![Packet Encapsulation](https://github.com/REZ-OAN/Rust_verse/blob/arg_parse/images/user_create_values.png)
+
+You can try your self with the `todo` command!!
+
--- arg_parse/src/args.rs
+++ arg_parse/src/args.rs
@@ -2,16 +2,125 @@ use clap::{
     Args,
     Parser,
     Subcommand
-};
-
-#[derive(Debug, Parser)]
-#[clap(author, version, about)]
-// defining the structure
-pub struct MainArgs{
-    /// The first argument!!
-    pub first_arg : String,
-    /// The first argument!!
-    pub second_arg : String,
-    /// The first argument!!
-    pub third_arg : String,
-}
+ };
+ 
+ 
+ #[derive(Debug, Parser)]
+ #[clap(author, version, about)]
+ // defining the structure
+ pub struct MainArgs{
+  // creating subcommand using a macro
+  #[clap(subcommand)]
+  pub entity_type : EntityType,
+ }
+ #[derive(Debug,Subcommand)]
+ pub enum EntityType{
+    /// create, update, delete or show users
+    User(UserCommand),
+    /// create, update, delete or show todos
+    Todos(TodoCommand),
+ }
+ 
+ 
+ #[derive(Debug,Args)]
+ pub struct UserCommand{
+    #[clap(subcommand)]
+    pub command: UserSubcommand,
+ }
+ #[derive(Debug,Args)]
+ pub struct TodoCommand{
+    #[clap(subcommand)]
+    pub command: TodoSubcommand,
+ }
+ #[derive(Debug,Subcommand)]
+ pub enum UserSubcommand{
+    /// Creates a new user
+    Create(CreateUser),
+    /// Updates a user
+    Update(UpdateUser),
+    /// Delete a User
+    Delete(DeleteUser),
+    /// Show  User Details
+    Show(ShowUser),
+ }
+ #[derive(Debug,Subcommand)]
+ pub enum TodoSubcommand{
+    /// Creates a new Todo
+    Create(CreateTodo),
+    /// Updates a Todo
+    Update(UpdateTodo),
+    /// Delete a Todo
+    Delete(DeleteTodo),
+    /// Show  Todo Details
+    Show(ShowTodo),
+ }
+ 
+ 
+ 
+ 
+ #[derive(Debug,Args)]
+ pub struct CreateUser{
+    ///  The name of the user
+    pub name: String,
+    /// The email of the user
+    pub email: String,
+    pub password: String,
+ }
+ #[derive(Debug,Args)]
+ pub struct UpdateUser{
+    /// The email of the user
+    pub email: String,
+    /// Update body  new_name
+    pub new_name : String,
+    /// Update body new_pass
+    pub new_pass : String,
+    /// Update body old_pass
+    pub old_pass : String,
+ }
+ #[derive(Debug,Args)]
+ pub struct DeleteUser{
+    /// The email of an existing user
+    pub email: String,
+ }
+ #[derive(Debug,Args)]
+ pub struct ShowUser{
+    /// The email of an existing user
+    pub email: String,
+ }
+ 
+ 
+ 
+ 
+ #[derive(Debug,Args)]
+ pub struct CreateTodo {
+    /// The index of the Todo
+    pub index: String,
+    /// The Description of the Todo
+    pub description: String,
+    /// The status of the Todo
+    #[clap(default_value="not_done_yet")]
+    pub status:String,
+ }
+ #[derive(Debug,Args)]
+ pub struct UpdateTodo{
+    /// The index of the Todo
+    pub index: String,
+    /// Update Body new_description
+    pub new_description: String,
+    /// Update Body status
+    pub status:String,
+ }
+ #[derive(Debug,Args)]
+ pub struct DeleteTodo{
+    /// The index of the Todo
+    pub index: String,
+ }
+ #[derive(Debug,Args)]
+ pub struct ShowTodo{
+    /// The index of Todo
+    pub index: String,
+ }
+ 
+ 
+ 
+ 
